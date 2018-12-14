d7a537ad5ef75bd742d5ee230122464e72ff339c
Ally Weatherly <Allymweatherly@gmail.com>
2018-12-14 09:45:09 -0500

    Class Walk through

---
+++ javaPhillip.js
@@ -0,0 +1,18 @@
+new Accordion('accordion1').init();
+
+/* My Variables*/
+var table = document.getElementById("mainTable");
+var nav = document.getElementById("sidNav");
+var card = document.getElementById("mainCard");
+var table = document.getElementById("mainTable")
+
+
+var header = document.getElementById("header"); //look on page for any element that has this ID
+console.log(header);
+
+header = document.querySelector("header"); //return first element that has class header on our page
+console.log(header);
+
+header = document.getElementsByClassName("header"); //gonna be a list with get elements part by class
+console.log(header);
+
